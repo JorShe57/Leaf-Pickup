b0ef06bb1a07eed6e9a8ac0bba1cb02e36c6114f
JorShe57 <jorshevel@gmail.com>
2025-10-08 08:45:34 -0400

    modal

--- api/streets.js
+++ api/streets.js
@@ -54,6 +54,7 @@ export default async function handler(req, res) {
         dateStarted: record.fields['Date Started'],
         status: record.fields['Status'],
         dateCompleted: record.fields['Date Completed'],
+        notes: record.fields['Notes'] || '',
       }));
   
       res.status(200).json({ records });
